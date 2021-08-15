dfef0d8724704d41d9a17fde310804f6e33dbf97
nateco01 <88977921+nateco01@users.noreply.github.com>
2021-08-15 14:20:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,23 @@ app.use(express.static('public'))
 const http = require('http').Server(app)
 const serverSocket = require('socket.io')(http)
 
-const porta = 3000
+
+const porta = process.env.PORT || 8000 //sera mudado
+
+const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : 'http://localhost'
 
 http.listen(porta, function(){
     console.log('Conexão bem sucedida, abra o seu navegador em http//localhost'+ porta)
+    
+    const portaStr = porta === 80 ?'' : ':' + porta
+
+    if(process.env.HEROKU_APP_NAME)
+    {
+        console.log('servidor iniciado abra o navegador em ' + host)
+    }else
+    {
+        console.log('servidor iniciado abra o navegador em ' + host + portaStr)
+    }
 })
 
 app.get('/', function(req, resp){
